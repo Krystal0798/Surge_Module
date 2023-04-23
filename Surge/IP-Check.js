250acf4c8277acf2792f0f4fdf10aa8cba56f04b
Goddess-Krystal <103553035+Goddess-Krystal@users.noreply.github.com>
2023-04-24 01:43:32 +0800

    Update IP-Check.js

--- Surge/IP-Check.js
+++ Surge/IP-Check.js
@@ -1,36 +1,41 @@
 /*
- * 由@congcong0806编写
- * 原脚本地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
- * 由@Rabbit-Spec修改
- * 更新日期：2022.08.14
- * 版本：1.5
- */
+脚本原作者：聪聪
+原脚本地址：https://raw.githubusercontent.com/congcong0806/surge-list/master/Script/ipcheck.js
+修改：TributePaulWalker
+Surge：
 
-let url = "http://ip-api.com/json"
+[Panel]
+网络信息 = script-name=网络信息, title="网络信息", content="请刷新", style=info, update-interval=60
+
+[Script]
+网络信息 = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/ipcheck.js
+
+*/
+
+let url = "http://ip-api.com/json/?lang=zh-CN"
 
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
+    let ip = jsonData.query
     let country = jsonData.country
     let emoji = getFlagEmoji(jsonData.countryCode)
     let city = jsonData.city
     let isp = jsonData.isp
-    let ip = jsonData.query
+    
   body = {
     title: "𝑫𝒂𝒕𝒂 𝒇𝒓𝒐𝒎 𝒊𝒑-𝒂𝒑𝒊.𝒄𝒐𝒎",
-    content: `IP：${ip}\nloc：${emoji} | ${country}\nisp：${isp} `,
-    icon: "xserve"
+    content: `IP: ${ip}\nloc: ${emoji} | ${country} - ${city}\nisp: ${isp}`,
+    icon: "xserve",
+    'icon-color': "#20B2AA"
   }
   $done(body);
 });
 
-function getFlagEmoji(countryCode) {
-      if (countryCode.toUpperCase() == 'TW') {
-    countryCode = 'CN'
-  }
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt())
-  return String.fromCodePoint(...codePoints)
 
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
 }
