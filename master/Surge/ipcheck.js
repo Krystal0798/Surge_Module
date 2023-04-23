e8c17ce65ec67454c312b7fae8a1647f16aaf563
Goddess-Krystal <103553035+Goddess-Krystal@users.noreply.github.com>
2023-04-24 01:38:41 +0800

    Update ipcheck.js

--- master/Surge/ipcheck.js
+++ master/Surge/ipcheck.js
@@ -24,9 +24,9 @@ $httpClient.get(url, function(error, response, data){
     
   body = {
     title: "𝑫𝒂𝒕𝒂 𝒇𝒓𝒐𝒎 𝒊𝒑-𝒂𝒑𝒊.𝒄𝒐𝒎",
-    content: `IP: ${ip}\nloc: ${emoji}${country} - ${city}\nisp: ${isp}`,
+    content: `IP: ${ip}\nloc: ${emoji} | ${country} - ${city}\nisp: ${isp}`,
     icon: "xserve",
-    'icon-color': "#3CB371"
+    'icon-color': "#20B2AA"
   }
   $done(body);
 });
