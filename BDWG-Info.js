b513280f03b7941c8e736c0e1356da3bcef35162
Goddess-Krystal <103553035+Goddess-Krystal@users.noreply.github.com>
2023-10-07 09:58:11 +0800

    Update and rename BWG-Info.js to BDWG-Info.js

--- BDWG-Info.js
+++ BDWG-Info.js
@@ -35,10 +35,10 @@ $httpClient.get(requestUrl, function(error, response, data){
     minutes = minutes > 9 ? minutes : "0" + minutes;
 
     $done({
-        title: `𝐁𝐚𝐧𝐝𝐰𝐚𝐠𝐨𝐧𝐇𝐨𝐬𝐭  𝑰𝒏𝒇𝒐  | ${hour}:${minutes}`,
+        title: `𝐁𝐚𝐧𝐝𝐰𝐚𝐠𝐨𝐧  𝑰𝒏𝒇𝒐  | 运行时间: ${hour}:${minutes}`,
         content: content.join("\n"),
-        icon: "server.rack",
-        "icon-color": "#3498db",
+        icon: "aqi.medium",
+        "icon-color": "#228B22",
     });
 });
 
