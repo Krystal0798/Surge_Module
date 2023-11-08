688ea5577e73339413fb3d3ae4bf59dc6972e5da
Goddess-Krystal <103553035+Goddess-Krystal@users.noreply.github.com>
2023-11-08 21:56:50 +0800

    Update panel-status.js

--- panel-status.js
+++ panel-status.js
@@ -15,7 +15,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","您的配置已重载成功😋","")
 };
 $done({
-    title:"𝑺𝒖𝒓𝒈𝒆 𝑷𝒓𝒐®      已运行: "+startTime,
+    title:"𝑺𝒖𝒓𝒈𝒆 𝑷𝒓𝒐®     已运行: "+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"      Rewrite:"+icon_status(rewrite_status.enabled)+"      Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"info.circle.fill",
    "icon-color":icon_s?"#f6c970":"#FF7500"
