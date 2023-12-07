bfedc16d4e1cd9da10236bfece6830fe27c56cf8
Goddess-Krystal <103553035+Goddess-Krystal@users.noreply.github.com>
2023-12-07 21:03:11 +0800

    Update Bdwg-info.js

--- Bdwg-info.js
+++ Bdwg-info.js
@@ -20,7 +20,7 @@ $httpClient.get(requestUrl, function(error, response, data){
     let content = [
         `IP: ${ipAddresses}`,
         `Dosage：${bytesToSize(dataCounter)} | ${bytesToSize(planMonthlyData)}`,
-        `Reset：${dataNextReset.getFullYear()}年${dataNextReset.getMonth() + 1}月${dataNextReset.getDate()}日`,
+        `Resets：${dataNextReset.getFullYear()}年${dataNextReset.getMonth() + 1}月${dataNextReset.getDate()}日`,
         `Plan: ${plan}`,
         `IDC: ${nodeDatacenter}`,
         `OS: ${os}`,
