b7723b9ac5fd7680ed3b9787269f352ae4226f13
Krystal0798 <103553035+Krystal0798@users.noreply.github.com>
2024-11-11 19:34:19 +0800

    Update serverinf6.js

--- serverinf6.js
+++ serverinf6.js
@@ -6,7 +6,7 @@ $httpClient.get(requestUrl, function(error, response, data){
     let jsonData = JSON.parse(data);
 
     // 获取所需的数据
-    let ipAddresses = jsonData.ip_addresses.join(', ');
+    let ipAddresses = jsonData.ip_addresses.map(ip => hideLastTwoDigits(ip)).join(', '); // Hide last two digits of each IP address
     let nodeDatacenter = jsonData.node_datacenter;
     let os = jsonData.os;
     let plan = jsonData.plan;
@@ -21,37 +21,9 @@ $httpClient.get(requestUrl, function(error, response, data){
         `IP: ${ipAddresses}`,
         `Dosage：${bytesToSize(dataCounter)} | ${bytesToSize(planMonthlyData)}`,
         `Resets：${dataNextReset.getFullYear()}年${dataNextReset.getMonth() + 1}月${dataNextReset.getDate()}日`,
+        `Plan: ${plan}`,
         `IDC: ${nodeDatacenter}`,
         `OS: ${os}`,
         `Disk: ${planDisk}`,
         `RAM: ${planRam}`
     ];
-
-    let now = new Date();
-    let hour = now.getHours();
-    let minutes = now.getMinutes();
-    hour = hour > 9 ? hour : "0" + hour;
-    minutes = minutes > 9 ? minutes : "0" + minutes;
-
-    $done({
-        title: `𝐁𝐚𝐧𝐝𝐰𝐚𝐠𝐨𝐧  𝑰𝒏𝒇𝒐 |  𝐑𝐮𝐧𝐭𝐢𝐦𝐞:  ${hour}:${minutes}`,
-        content: content.join("\n"),
-        icon: "server.rack",
-        "icon-color": "#3498db",
-    });
-});
-
-function getArgs() {
-    return Object.fromEntries(
-        $argument
-            .split("&")
-            .map((item) => item.split("="))
-    );
-}
-
-function bytesToSize(bytes) {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return '0 Byte';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
